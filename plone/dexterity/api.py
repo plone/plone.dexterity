02deb010a7e0d1dfec8acd412b796fa28f8c76f3
Martin Aspeli <optilude@gmail.com>
2008-06-28 20:21:51 +0000

    No need for import tricks anymore
    svn path=/plone.dexterity/trunk/; revision=21298

--- plone/dexterity/api.py
+++ plone/dexterity/api.py
@@ -1,12 +1,2 @@
-import zope.deferredimport
-
-zope.deferredimport.defineFrom(
-    'plone.supermodel.directives',
-    'Schema', 'model'
-)
-
-zope.deferredimport.defineFrom(
-    'plone.dexterity.content'
-    'Item', 'Container'
-)
-
+from plone.supermodel.directives import Schema, model
+from plone.dexterity.content import Item, Container
