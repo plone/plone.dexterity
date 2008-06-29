b934fae5e54f501cc619616ca5998a3ef50174cf
Martin Aspeli <optilude@gmail.com>
2008-06-29 16:12:41 +0000

    Note to self
    svn path=/plone.dexterity/trunk/; revision=21314

--- plone/dexterity/tests/test_content_integration.py
+++ plone/dexterity/tests/test_content_integration.py
@@ -1,8 +1,10 @@
 import unittest
 
 # TODO: End to end tests that ensure components are properly wired up
+#  - for now, we have some tests in example.dexterity, but we should have
+#    more specific tests here.
 
 def test_suite():
     suite = unittest.TestSuite()
-    # suite.addTest(unittest.makeSuite(TestUtils))
+    # suite.addTest(unittest.makeSuite(TestIntegration))
     return suite
