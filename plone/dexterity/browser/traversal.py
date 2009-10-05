670e2fcd3a691c6519fced890d3cf662716ecbe7
Martin Aspeli <optilude@gmail.com>
2009-10-05 15:37:44 +0000

    Hopefully temporary workaround for stupid acquisition-related problem with WebDAV PUT and NullResources
    svn path=/plone.dexterity/trunk/; revision=30204

--- plone/dexterity/browser/traversal.py
+++ plone/dexterity/browser/traversal.py
@@ -8,6 +8,9 @@ try:
 except ImportError:
     from ZPublisher.BaseRequest import DefaultPublishTraverse
 
+from Acquisition import aq_inner, aq_parent
+from Acquisition.interfaces import IAcquirer
+from webdav.NullResource import NullResource
 
 class DexterityPublishTraverse(DefaultPublishTraverse):
     """Override the default browser publisher to make WebDAV work for
@@ -22,7 +25,24 @@ class DexterityPublishTraverse(DefaultPublishTraverse):
     def __init__(self, context, request):
         self.context = context
         self.request = request
-
+    
+    def publishTraverse(self, request, name):
+        
+        context = aq_inner(self.context)
+        
+        defaultTraversal = super(DexterityPublishTraverse, self).publishTraverse(request, name)
+        
+        # If this is a WebDAV request, don't acquire things
+        if (getattr(request, 'maybe_webdav_client', False) and
+            request.get('REQUEST_METHOD', 'GET') not in ('GET', 'POST',)
+        ):
+            if IAcquirer.providedBy(defaultTraversal):
+                parent = aq_parent(aq_inner(defaultTraversal))
+                if parent is not None and parent is not context:
+                    return NullResource(self.context, name, request).__of__(self.context)
+        
+        return defaultTraversal
+    
     def browserDefault(self, request):
         
         # If this is not a WebDAV request, we don't want to give a 
