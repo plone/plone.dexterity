c6d93e027608cf87274157affe38e5eaf802df92
tisto <tisto@plone.org>
2013-10-10 16:30:40 +0200

    Move docs/HISTORY.txt to CHANGES.rst to apply to Plone coredev best practices.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='plone.dexterity',
       version=version,
       description="Flexible CMF content",
       long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+                       open("CHANGES.rst")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
