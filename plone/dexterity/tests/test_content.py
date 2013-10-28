7b9679ffbf7f588e4c6907db5e21f962d06b7293
Thomas Desvenain <thomas.desvenain@gmail.com>
2013-10-28 10:10:19 +0100

    tests pass everywhere in the world...

--- plone/dexterity/tests/test_content.py
+++ plone/dexterity/tests/test_content.py
@@ -531,10 +531,10 @@ class TestContent(MockTestCase):
         self.assertEqual(i.Contributors(), ('admin',))
         self.assertEqual(i.format, 'text/plain')
         self.assertEqual(i.effective_date, DateTime('08/20/2010'))
-        self.assertEqual(i.EffectiveDate()[:10], '2010-08-20')
+        self.assertEqual(i.EffectiveDate(zone='GMT+2')[:10], '2010-08-20')
         self.assertEqual(i.effective(), DateTime('08/20/2010'))
         self.assertEqual(i.expiration_date, DateTime('07/09/2013'))
-        self.assertEqual(i.ExpirationDate()[:10], '2013-07-09')
+        self.assertEqual(i.ExpirationDate(zone='GMT+2')[:10], '2013-07-09')
         self.assertEqual(i.expires(), DateTime('07/09/2013'))
         self.assertEqual(i.language, 'de')
         self.assertEqual(i.Language(), 'de')
@@ -564,10 +564,10 @@ class TestContent(MockTestCase):
             )
 
         self.assertEqual(i.effective_date, DateTime('08/20/2010'))
-        self.assertEqual(i.EffectiveDate()[:10], '2010-08-20')
+        self.assertEqual(i.EffectiveDate(zone='GMT+2')[:10], '2010-08-20')
         self.assertEqual(i.effective(), DateTime('08/20/2010'))
         self.assertEqual(i.expiration_date, DateTime('07/09/2013'))
-        self.assertEqual(i.ExpirationDate()[:10], '2013-07-09')
+        self.assertEqual(i.ExpirationDate(zone='GMT+2')[:10], '2013-07-09')
         self.assertEqual(i.expires(), DateTime('07/09/2013'))
         self.assertEqual(i.creation_date, i.created())
         self.assertEqual(i.CreationDate()[:19], i.creation_date.ISO()[:19])
@@ -584,19 +584,19 @@ class TestContent(MockTestCase):
             description=u'One of the most magnificent birds.',
             subject=u'Penguins',
             contributors=u'admin',
-            effective_date=datetime(2010, 8, 20, 23, 59, 59, 0, timezone('US/Eastern')),
-            expiration_date=datetime(2013, 7, 9, 23, 59, 59, 0, timezone('US/Eastern')),
+            effective_date=datetime(2010, 8, 20, 12, 59, 59, 0, timezone('US/Eastern')),
+            expiration_date=datetime(2013, 7, 9, 12, 59, 59, 0, timezone('US/Eastern')),
             format='text/plain',
             language='de',
             rights='CC',
             )
 
-        self.assertEqual(i.effective_date, DateTime('08/20/2010 23:59:59 GMT-5'))
-        self.assertEqual(i.EffectiveDate(), '2010-08-21 06:59:59')
-        self.assertEqual(i.effective(), DateTime('08/20/2010 23:59:59 GMT-5'))
-        self.assertEqual(i.expiration_date, DateTime('07/09/2013 23:59:59 GMT-5'))
-        self.assertEqual(i.ExpirationDate(), '2013-07-10 06:59:59')
-        self.assertEqual(i.expires(), DateTime('07/09/2013 23:59:59 GMT-5'))
+        self.assertEqual(i.effective_date, DateTime('08/20/2010 12:59:59 GMT-5'))
+        self.assertEqual(i.EffectiveDate(zone='GMT+2'), '2010-08-20 19:59:59')
+        self.assertEqual(i.effective(), DateTime('08/20/2010 12:59:59 GMT-5'))
+        self.assertEqual(i.expiration_date, DateTime('07/09/2013 12:59:59 GMT-5'))
+        self.assertEqual(i.ExpirationDate(zone='GMT+2'), '2013-07-09 19:59:59')
+        self.assertEqual(i.expires(), DateTime('07/09/2013 12:59:59 GMT-5'))
         self.assertEqual(i.creation_date, i.created())
         self.assertEqual(i.CreationDate(), i.creation_date.ISO())
         self.assertEqual(i.modification_date, i.creation_date)
