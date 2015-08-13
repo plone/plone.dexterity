bd7a6a4302a6bb502281e46230bcf0b8b23f47b7
Maurits van Rees <maurits@vanrees.org>
2015-08-13 18:46:25 +0200

    Added classifiers to setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -17,10 +17,14 @@ setup(
     description=short_description,
     long_description=long_description,
     # Get more strings from
-    # http://www.python.org/pypi?%3Aaction=list_classifiers
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: 5.0",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     keywords='',
