c1129e93bcd3128ac9ee7c105f93f2bf7c11404f
esteele <eric@esteele.net>
2015-09-18 09:52:42 +0200

    Fix registry id

--- plone/dexterity/browser/edit.py
+++ plone/dexterity/browser/edit.py
@@ -48,7 +48,7 @@ class DefaultEditForm(DexterityExtensibleForm, form.EditForm):
         if portal_type is not None:
             registry = getUtility(IRegistry)
             use_view_action = registry.get(
-                'plone.types_view_action_in_listings', [])
+                'plone.types_use_view_action_in_listings', [])
             if portal_type in use_view_action:
                 view_url = view_url + '/view'
         return view_url
