45d832f6ffa8dea2b2ec93d7d590eedfd7aa7e8e
Maurits van Rees <maurits@vanrees.org>
2015-10-28 01:40:37 +0100

    Removed Plone 4.3 and Python 2.6 classifiers from setup.py.
    Only coredev 5.0 uses this master branch.

--- setup.py
+++ setup.py
@@ -17,13 +17,11 @@ setup(
     description=short_description,
     long_description=long_description,
     # Get more strings from
-    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         "Framework :: Plone",
-        "Framework :: Plone :: 4.3",
         "Framework :: Plone :: 5.0",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
