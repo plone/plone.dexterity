0e6c3284ba5148d61d56f9e60f743e0855f8ef40
Maurits van Rees <maurits@vanrees.org>
2016-05-05 00:46:13 +0200

    Preparing release 2.2.7
    [ci skip]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-version = '2.2.7.dev0'
+version = '2.2.7'
 
 short_description = """\
 Framework for content types as filesystem code and TTW (Zope/CMF/Plone)\
