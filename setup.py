24768010185bdbbbfe2eae12cb28cb82ac9afae2
Maurits van Rees <maurits@vanrees.org>
2017-06-03 21:17:21 +0200

    Removed old classifiers.
    [ci skip]

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(
     # https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         "Framework :: Plone",
-        "Framework :: Plone :: 5.0",
         "Framework :: Plone :: 5.1",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
