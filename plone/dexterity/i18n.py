2551baa623033123b0c9673490ef963fe614cb86
Mikel Larreategi <mlarreategi@codesyntax.com>
2018-03-20 12:33:10 +0100

    use plone as i18n domain

--- plone/dexterity/i18n.py
+++ plone/dexterity/i18n.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 import zope.i18nmessageid
-MessageFactory = zope.i18nmessageid.MessageFactory("plone.dexterity")
+MessageFactory = zope.i18nmessageid.MessageFactory("plone")
