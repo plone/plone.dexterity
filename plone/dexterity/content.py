e6bd7fe602efb6b3ef1f3c49ce7e22ac2f08f2b6
Roel Bruggink <roel@jaroel.nl>
2019-12-10 17:31:46 +0100

    maybe you're gonna be that one that

--- plone/dexterity/content.py
+++ plone/dexterity/content.py
@@ -766,35 +766,37 @@ class Container(
         CMFOrderedBTreeFolderBase.__init__(self, id)
         DexterityContent.__init__(self, id, **kwargs)
 
-    def __getattr__(self, name):
-        try:
-            return DexterityContent.__getattr__(self, name)
-        except AttributeError:
-            pass
+    # def __getattr__(self, name):
+    #     # if name[0] != '_':
+    #     #     print('Container.__getattr__', self, name)
+    #     try:
+    #         return DexterityContent.__getattr__(self, name)
+    #     except AttributeError:
+    #         pass
 
-        # Be specific about the implementation we use
-        return CMFOrderedBTreeFolderBase.__getattr__(self, name)
+    #     # Be specific about the implementation we use
+    #     return CMFOrderedBTreeFolderBase.__getattr__(self, name)
 
-    def __delattr__(self, name):
-        try:
-            super(Container, self).__delattr__(name)
-        except AttributeError:  # delete the item instead
-            del self[name]
+    # def __delattr__(self, name):
+    #     try:
+    #         super(Container, self).__delattr__(name)
+    #     except AttributeError:  # delete the item instead
+    #         del self[name]
 
-    def __setattr__(self, name, value):
-        # If we have an existing attribute, just set it.
-        # We'll check this first, so we don't check the tree unneeded.
-        if name in self.__dict__:
-            super(Container, self).__setattr__(name, value)
+    # def __setattr__(self, name, value):
+    #     # If we have an existing attribute, just set it.
+    #     # We'll check this first, so we don't check the tree unneeded.
+    #     if name in self.__dict__:
+    #         super(Container, self).__setattr__(name, value)
 
-        # if we have a n item, set that
-        elif '_tree' in self.__dict__ and name in self:
-            del self[name]
-            self[name] = value
+    #     # if we have a n item, set that
+    #     elif '_tree' in self.__dict__ and name in self:
+    #         del self[name]
+    #         self[name] = value
 
-        # else we'll set an attribute.
-        else:
-            super(Container, self).__setattr__(name, value)
+    #     # else we'll set an attribute.
+    #     else:
+    #         super(Container, self).__setattr__(name, value)
 
     def _delObject(self, name, *args, **kwargs):
         super(Container, self)._delObject(name, *args, **kwargs)
