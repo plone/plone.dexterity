79e127bd762c7b53081e26779d5d6387b233a0ea
Maurits van Rees <maurits@vanrees.org>
2022-07-01 11:27:00 +0200

    Removed Plone 5.2 related classifiers.

--- setup.py
+++ setup.py
@@ -32,18 +32,14 @@ setup(
     version=version,
     description=short_description,
     long_description=long_description,
-    # Get more strings from
-    # https://pypi.org/classifiers/
+    # Get more strings from https://pypi.org/classifiers/
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Framework :: Plone",
-        "Framework :: Plone :: 5.2",
         "Framework :: Plone :: 6.0",
         "Framework :: Plone :: Core",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
