cfcc1e9af2595c26dfccd0f1c52de69676306bd2
Maurits van Rees <maurits@vanrees.org>
2022-11-24 12:37:27 +0100

    Require Python 3.8 as minimum.

--- setup.py
+++ setup.py
@@ -39,7 +39,6 @@ setup(
         "Framework :: Plone :: Core",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
@@ -57,7 +56,7 @@ setup(
     namespace_packages=["plone"],
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     install_requires=[
         "DateTime>=4.0.1",
         "plone.alterego",
